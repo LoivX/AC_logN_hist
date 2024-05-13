0782a0eaa56a63ac31822a04d4d7f2188a7401da
LoivX <simone.viol472@gmail.com>
2024-05-13 07:36:05 +0200

    added the concatenation of tables instead of concatenation of data

--- hist_logN(fits).py
+++ hist_logN(fits).py
@@ -5,33 +5,35 @@ import matplotlib.pyplot as plt
 import tarfile
 import tkinter as tk
 from tkinter import filedialog
+from astropy.table import Table, vstack
 
 
 # Initialize an empty list to store the extracted data
-data_list = []
+table_list = []
 
 ''' FUNCTIONS '''
-# Extract data from .fits file and append to data list
+# Extract data from .fits file
 def extract_data_from_fits():
     file_path = filedialog.askopenfilename(filetypes=[("FITS Files", "*.fits")])
     print('file selected succesfully: ', file_path)
     
     with fits.open(file_path) as hdul:
         # Extract the table
-        table_hdu = hdul[1]
-        data = table_hdu.data
-    
-    # Append the extracted data to the data list
-    data_list.append(data)
-    
+        table = hdul[1]
+        # Convert the table to an astropy table
+        table = Table(table.data)
+
+        # Append the table to the list
+        table_list.append(table)
+        print(table_list)
 
 # Merges the data collected and creates a histogram of logN for CIV systems
-def create_histogram(data_list):
-    # Merge the extracted data using vstack
-    data = np.vstack(data_list)
+def create_histogram(table_list):
+    # Merge the tables using vstack
+    table = np.vstack(table_list)
     
     # Extract CIV systems
-    data_CIV = data[data['series'] == 'CIV']    
+    data_CIV = table[table['series'] == 'CIV']    
 
     # Plot histogram of logN
     plt.hist(data_CIV['logN'], bins=10)
@@ -40,7 +42,7 @@ def create_histogram(data_list):
     plt.title('Histogram of logN')
     plt.show()
 
-# Create a GUI window
+# Create the GUI window
 def GUI():
     # Create the GUI window
     root = tk.Tk()
@@ -50,7 +52,7 @@ def GUI():
     button.pack()
 
     # Button to create histogram
-    button = tk.Button(root, text="Create histogram", command=lambda: create_histogram(data_list))
+    button = tk.Button(root, text="Create histogram", command=lambda: create_histogram(table_list))
     button.pack()
 
     # Run the GUI window
@@ -61,3 +63,4 @@ def GUI():
 ''' MAIN CODE '''
 GUI()
 
+''' trova un modo per mergiare i dati estratti da più file .fits'''
